54e5c6279d732948ae2820011c52a690ccd84f5f
Faris Zulkiffli <22FTT1517@student.pb.edu.bn>
2024-04-07 10:46:45 +0800

    Add new routes to app-routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,20 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+  },
+  {
+    path: 'aboutus',
+    loadChildren: () => import('./aboutus/aboutus.module').then( m => m.AboutusPageModule)
+  },
+  {
+    path: 'alert-history',
+    loadChildren: () => import('./alert-history/alert-history.module').then( m => m.AlertHistoryPageModule)
+  },
+  {
+    path: 'community',
+    loadChildren: () => import('./community/community.module').then( m => m.CommunityPageModule)
+  },
+  
 ];
 @NgModule({
   imports: [
