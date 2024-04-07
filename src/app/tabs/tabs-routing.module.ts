6056cd91ee1ce7db0d7aa7045dc56a46b53776de
Faris Zulkiffli <22FTT1517@student.pb.edu.bn>
2024-04-07 10:55:46 +0800

    adding routes to link for all tabs

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -20,6 +20,22 @@ const routes: Routes = [
         loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
       },
       {
+        path: 'pfpage2',
+        loadChildren: () => import('../pfpage2/pfpage2.module').then(m => m.Pfpage2PageModule)
+      },
+      {
+        path: 'aboutus',
+        loadChildren: () => import('../aboutus/aboutus.module').then(m => m.AboutusPageModule)
+      },
+      {
+        path: 'alert-history',
+        loadChildren: () => import('../alert-history/alert-history.module').then(m => m.AlertHistoryPageModule)
+      },
+      {
+        path: 'community',
+        loadChildren: () => import('../community/community.module').then(m => m.CommunityPageModule)
+      },
+      {
         path: '',
         redirectTo: '/tabs/tab1',
         pathMatch: 'full'
